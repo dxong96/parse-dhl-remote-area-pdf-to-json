83d78286e3d92a82b6cf4f53cd2505209dc11c33
Dexing <ongdexing@gmail.com>
2025-06-29 12:49:40 +0800

    Fix issues using the library in commonjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,12 +2,23 @@
 import typescript from "@rollup/plugin-typescript";
 import json from "@rollup/plugin-json";
 import {dts} from "rollup-plugin-dts";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default  [
     {
         input: 'src/index.ts',
         output: [
             { file: 'build/index.cjs', format: 'cjs' },
+        ],
+        plugins: [
+            commonjs(),
+            typescript(),
+            json()
+        ],
+    },
+    {
+        input: 'src/index.ts',
+        output: [
             { file: 'build/index.mjs', format: 'es' }
         ],
         plugins: [
@@ -17,8 +28,11 @@ export default  [
     },
     {
         // path to your declaration files root
-        input: './build/dts/src/index.d.ts',
-        output: [{ file: 'build/index.d.ts', format: 'es' }],
+        input: './build/types/mjs/src/index.d.ts',
+        output: [
+            { file: 'build/index.d.mts', format: 'es' },
+            { file: 'build/index.d.cts', format: 'cjs' }
+        ],
         plugins: [dts()],
-    },
+    }
 ];
